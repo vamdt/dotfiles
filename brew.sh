36038f073d0e76991ce4fdb96c7da3c44ccd727f
vamdt <liang6688267@gmail.com>
2014-06-12 18:34:08 +0800

    Add brew install

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+which -s brew
+if [[ $? != 0 ]]; then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew update
+
+brew install htop screenfetch mysql55 nmap sqlmap the_silver_searcher \
+node tree redis zsh git 
+
+brew tap caskroom/cask
+brew tap caskroom/versions
+brew update
+brew install brew-cask
+brew cask install dropbox evernote mplayerx sequel-pro sublime-text3 firefox skype
