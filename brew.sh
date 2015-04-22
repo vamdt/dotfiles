c7f03f5b5f9ac3a77856ea55562310fc3feeb80c
vamdt <liang6688267@gmail.com>
2015-04-22 14:26:05 +0800

    Complete brewsh

--- brew.sh
+++ brew.sh
@@ -10,12 +10,13 @@ brew tap homebrew/dupes
 brew tap homebrew/versions
 brew tap homebrew/homebrew-php
 brew update
+brew upgrade
 
-brew install htop screenfetch mysql55 nmap sqlmap the_silver_searcher \
-node tree redis zsh git 
+brew install htop screenfetch mysql56 nmap sqlmap the_silver_searcher \
+node tree redis zsh git coreutils findutils imagemagick rename mutt w3m urlview
 
 brew tap caskroom/cask
 brew tap caskroom/versions
 brew update
 brew install brew-cask
-brew cask install dropbox evernote mplayerx sequel-pro sublime-text3 firefox skype
+brew cask install dropbox evernote mplayerx sequel-pro sublime-text3 firefox skype iterm2
