c4aa6da5e857416cde8baed84830ea917587ebc4
vamdt <liang6688267@gmail.com>
2016-01-31 13:31:11 +0800

    Update sublime bootstrap

--- sublime.sh
+++ sublime.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+os=$(uname -s)
 cd $HOME
-MINE_PROJECTS_PATH="$HOME/projects/mine"
+MINE_PROJECTS_PATH="$HOME/repos/vamdt"
 SUBLIME_SETTINGS_PATH="$MINE_PROJECTS_PATH/sublime-text-settings"
-SUBLIME_DATA_PATH="$HOME/Library/Application Support/Sublime Text 3"
+if [ $os == "Darwin" ]; then
+  SUBLIME_DATA_PATH="$HOME/Library/Application Support/Sublime Text 3"
+elif [ $os == "Linux" ]; then
+  SUBLIME_DATA_PATH="$HOME/.config/sublime-text-3"
+else
+  echo "$os"
+  exit 1
+fi
+
 
 mkdir -p $MINE_PROJECTS_PATH
 
@@ -15,4 +24,7 @@ else
 fi
 
 ln -sf "$SUBLIME_DATA_PATH" "$HOME/.subl"
+if [[ -d "$SUBLIME_DATA_PATH/Packages/User" ]]; then
+  rm -rf "$SUBLIME_DATA_PATH/Packages/User"
+fi
 ln -sf "$SUBLIME_SETTINGS_PATH" "$SUBLIME_DATA_PATH/Packages/User"
