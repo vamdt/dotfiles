2a0322d414d84f57560f4cbcef112806e7b6b82f
vamdt <liang6688267@gmail.com>
2016-08-08 08:26:12 +0000

    Add prezto bootstrap shell

---
+++ prezto.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+set -xe
+# go to home dir
+cd "${ZDOTDIR:-$HOME}"
+# clone vamdt/prezto:v
+git clone --recursive https://github.com/vamdt/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Create a new Zsh configuration by copying the Zsh configuration files provided
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# switch to zsh
+sudo chsh -s /bin/zsh ${whoami}
+set +xe
